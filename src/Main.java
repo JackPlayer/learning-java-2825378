4a208765a96d94c54d694307da5487009101f679
Jack Player <jackson.m.player@gmail.com>
2022-04-01 21:21:31 -0700

    Implement salaryCalculator

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,24 @@
 public class Main {
 
     public static void main(String[] args) {
-        
+        System.out.println("Yearly Salary: " + calculateYearlySalary(40, 15.00));
+        System.out.println("Yearly Salary (10 vacation days): " + calculateYearlySalary(40, 15.00, 10));
     }
 
+    public static double calculateYearlySalary(int hoursPerWeek, double payPerHour) {
+        if (hoursPerWeek < 0 || payPerHour < 0) return 0;
+
+        final int NUM_WEEKS = 52;
+        double payPerWeek = hoursPerWeek * payPerHour;
+        return payPerWeek * NUM_WEEKS;
+    }
+
+    public static double calculateYearlySalary(int hoursPerWeek, double payPerHour, int vacationDays) {
+        if (hoursPerWeek < 0 || payPerHour < 0) return 0;
+
+        final int NUM_WEEKS = 52;
+        double payPerWeek = hoursPerWeek * payPerHour;
+        double deduction = (payPerHour * 8) * vacationDays;
+        return payPerWeek * NUM_WEEKS - deduction;
+    }
 }
